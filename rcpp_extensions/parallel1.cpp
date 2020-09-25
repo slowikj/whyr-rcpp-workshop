ac6e505f724f967a5d144c223f81d9fa3e266509
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-09-25 12:38:36 +0200

    implemented parallelization

--- rcpp_extensions/parallel1.cpp
+++ rcpp_extensions/parallel1.cpp
@@ -3,21 +3,28 @@
 #include <Rcpp.h>
 #include <RcppParallel.h>
 
-struct MyWorker : RcppParallel::Worker {
-  RVector<int> input_;
-  RVector<int> output_;
+struct MyWorker : public RcppParallel::Worker {
+  RcppParallel::RVector<int> input_;
+  RcppParallel::RVector<int> output_;
   
-  Worker(IntegerVector input, IntegerVector output) 
+  MyWorker(Rcpp::IntegerVector input, Rcpp::IntegerVector output) :
+    input_(RcppParallel::RVector<int>(input)),
+    output_(RcppParallel::RVector<int>(output)) {};
     
-  operator()(std::size_t begin, sitd::size_t end) {
-    
+  void operator()(std::size_t begin, std::size_t end) {
+    for(int i = begin; i < end; i++) {
+      output_[i] = input_[i] + 5;
+    }
   } 
 };
 
 //[[Rcpp::export]]
-IntegerVector add5(IntegerVector x) {
+Rcpp::IntegerVector add5(Rcpp::IntegerVector x) {
+  Rcpp::IntegerVector output(x.size());
   
-  MyWorker worker();
+  MyWorker worker(x, output);
   
   RcppParallel::parallelFor(0, x.size(), worker);
+  
+  return output;
 }
